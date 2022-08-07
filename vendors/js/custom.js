4044f3c8b53c52d485e38512aedafaffc6307288
Md.Ashikur Rahman Ashik <96907592+ashikur-18@users.noreply.github.com>
2022-08-07 21:21:06 +0600

    Add files via upload

---
+++ vendors/js/custom.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+   
+  // Moble Menus JS
+      // Mobile Menus JS
+      $('#menu_icon').click(function(){
+          $('.menuber_area ul').slideToggle()
+      })
+  });
